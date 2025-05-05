24a1f99fe78861f4a16fb75111ff789176125415
Chris <chris79gp@outlook.com>
2025-05-05 16:05:55 +0100

    Test: add unit tests for error handling in `Knight.travail()`

--- __tests__/Knight.test.js
+++ __tests__/Knight.test.js
@@ -52,5 +52,39 @@ describe("Knight", () => {
 			expect(path[0]).toEqual([3, 3]);
 			expect(path[3]).toEqual([4, 3]);
 		});
+		describe("throws an error for invalid coordinates", () => {
+			test("if a coordinate is out of bounds", () => {
+				const outOfBounds = [-1, 0];
+				expect(() => {
+					knight.travail(outOfBounds, [1, 2]);
+				}).toThrowError(new Error(`Invalid coordinate: start: [${outOfBounds}]`));
+			});
+			test("if a coordinate is not a number", () => {
+				const notNumber = ["zero", 0];
+				expect(() => {
+					knight.travail(notNumber, [1, 2]);
+				}).toThrowError(new Error(`Invalid coordinate: start: [${notNumber}]`));
+			});
+			test("if a coordinate is not an array", () => {
+				const notArray = "coords";
+				expect(() => {
+					knight.travail(notArray, [1, 2]);
+				}).toThrowError(new Error(`Invalid coordinate: start: [${notArray}]`));
+			});
+			test("for the correct coordinate", () => {
+				const invalidCoord = [-1, 0];
+				expect(() => {
+					knight.travail(invalidCoord, [1, 2]);
+				}).toThrowError(new Error(`Invalid coordinate: start: [${invalidCoord}]`));
+				expect(() => {
+					knight.travail([1, 2], invalidCoord);
+				}).toThrowError(new Error(`Invalid coordinate: end: [${invalidCoord}]`));
+				expect(() => {
+					knight.travail(invalidCoord, invalidCoord);
+				}).toThrowError(
+					new Error(`Invalid coordinates: start: [${invalidCoord}], end: [${invalidCoord}]`)
+				);
+			});
+		});
 	});
 });
